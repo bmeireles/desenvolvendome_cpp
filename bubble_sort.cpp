93d2e7aa936e0c5871c03531b79372bdb2a0f361
Ludmila Meireles <ludmilabmeireles@gmail.com>
2022-10-08 18:56:42 -0300

    bubble sort completed

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -11,35 +11,36 @@
 using namespace std;
  
 // A function to implement bubble sort
-void bubbleSort(int arr[], int n)
+void bubbleSort(int arr[], int n)//function bubblesort to order an array, receives as parameters 
+//an array, the size n of the array
 {
     int i, j;
-    for (i = 0; i < n - 1; i++)
+    for (i = 0; i < n - 1; i++)//for each element of the array
  
-        // Last i elements are already
-        // in place
-        for (j = 0; j < n - i - 1; j++)
-            if (arr[j] > arr[j + 1])
-                swap(arr[j], arr[j + 1]);
+        for (j = 0; j < n - i - 1; j++)//for each element of the array minus the i elements already
+        //in place
+            if (arr[j] > arr[j + 1])//if the value in index j is greater than the value in
+            //the next index
+                swap(arr[j], arr[j + 1]); //swap both values
 }
  
-// Function to print an array
-void printArray(int arr[], int size)
+void printArray(int arr[], int size)//function to print an array, receives as parameters an array
+//and its size
 {
     int i;
-    for (i = 0; i < size; i++)
-        cout << arr[i] << " ";
+    for (i = 0; i < size; i++)//for each element in the array
+        cout << arr[i] << " ";//print the value in that index
     cout << endl;
 }
  
 // Driver code
 int main()
 {
-    int arr[] = { 5, 1, 4, 2, 8};
-    int N = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, N);
+    int arr[] = { 5, 1, 4, 2, 8}; //creating an array
+    int N = sizeof(arr) / sizeof(arr[0]);// finding the size os the array
+    bubbleSort(arr, N);//calling the function bubblesort
     cout << "Sorted array: \n";
-    printArray(arr, N);
+    printArray(arr, N);//calling the function to print the sorted array
     return 0;
 }
 // This code is contributed by rathbhupendra
