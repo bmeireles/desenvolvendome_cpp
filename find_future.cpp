0c9588a8e83d20e0860b8c1dd3ba24a701eb4434
Ludmila Meireles <ludmilabmeireles@gmail.com>
2022-12-06 09:15:35 -0300

    function to compare years months and days added

--- find_future.cpp
+++ find_future.cpp
@@ -55,10 +55,11 @@ void printDates(string dates[], int n)
 string searchList(string array[], int numElems, string value) { //function to to through the array
 //and find the closest value. Receives as parameters an array, the number of elements in the array,
 //and the value we want to look for in the array
-    int diff = abs(stoi(array[0]) - stoi(value)); //the difference value starts with the first value of
+    int diff = abs(stoi(array[0].substr(6,4)) - stoi(value.substr(6,4))); //the difference value starts with the first value of
 	//the array minus the value we want to find. The smaller the difference, the closest they are.
 	int idx = 0; //variable to save the index of the value in the array
-    for (int i = 0; i < numElems; i++) { //for every value in the array
+	int i = 0;
+    for (int i; i < numElems; i++) { //for every value in the array
         if (abs(stoi(array[i]) - stoi(value)) < diff) { //if the difference between the the element in the list 
 		//and the value is smaller than the current difference
             diff = abs(stoi(array[i]) - stoi(value)); //save that value as the new difference
@@ -78,6 +79,82 @@ string nextClosestDate(string arr[], string q, int n) //n is the size of the arr
 	// - - - If there is only one value, return this element as the closest
 	// - - - If there is more than one element, compare day between these elements
 	// - If there is only one value, return this element
+
+	//Compare years: find the closest year in the array
+	int diff = abs(stoi(arr[0].substr(6,4)) - stoi(q.substr(6,4))); //the difference value starts with the first value of
+	//the array minus the value we want to find. The smaller the difference, the closest they are.
+	int idx = 0; //variable to save the index of the value in the array
+	int i = 0;
+    for (int i; i < n; i++) { //for every value in the array
+        if (abs(stoi(arr[i].substr(6,4)) - stoi(q.substr(6,4))) < diff) { //if the difference between the the element in the list 
+		//and the value is smaller than the current difference
+            diff = abs(stoi(arr[i].substr(6,4)) - stoi(q.substr(6,4))); //save that value as the new difference
+            idx = i; //save the index of the element in the array
+        }
+    }
+    //arr[idx]; // the closest element according to the year
+	string closest_year = arr[idx].substr(6,4) //the closest year
+
+	//Go through the array again with the closest year
+	string years_arr[n];
+	int i = 0;
+	for (int i; i < n; i++) {
+		if (arr[i].substr(6,4) == closest_year) {
+			years_arr[i] = arr[i];
+		}
+	}
+
+	// - If there is only one value, return this element
+	int size_years = sizeof(years_arr)/sizeof(years_arr[0]);
+	if (size_years == 1) { //if there is only one element, this is the closest of all
+		return years_arr[0]; //return the closest date
+	}
+	else {//If there is more than one value for the same year, compare month between these elements
+		int diff_month = abs(stoi(years_arr[0].substr(3,2)) - stoi(q.substr(3,2))); //the difference value starts with the first value of
+		//the array minus the value we want to find. The smaller the difference, the closest they are.
+		int idx_month = 0; //variable to save the index of the value in the array
+		int i = 0;
+   		for (int i; i < size_years; i++) { //for every value in the array
+       		if (abs(stoi(years_arr[i].substr(3,2)) - stoi(q.substr(3,2))) < diff_month) { //if the difference between the the element in the list 
+			//and the value is smaller than the current difference
+           		diff_month = abs(stoi(years_arr[i].substr(3,2)) - stoi(q.substr(3,2))); //save that value as the new difference
+            	idx_month = i; //save the index of the element in the array
+        	}
+    	}
+
+		//years_arr[idx_month]; // the closest element according to the year and month
+		string closest_month = years_arr[idx_month].substr(3,2) //the closest month
+
+		//Go through the array again with the closest month
+		string months_arr[n];
+		int i = 0;
+		for (int i; i < n; i++) {
+			if (years_arr[i].substr(3,2) == closest_month) {
+				months_arr[i] = years_arr[i];
+			}
+		}
+
+		// - If there is only one value, return this element
+		int size_months = sizeof(months_arr)/sizeof(months_arr[0]);
+		if (size_months == 1) { //if there is only one element, this is the closest of all
+			return months_arr[0]; //return the closest date
+		else {//If there is more than one value for the same year, compare month between these elements
+			int diff_day = abs(stoi(months_arr[0].substr(0,2)) - stoi(q.substr(0,2))); //the difference value starts with the first value of
+			//the array minus the value we want to find. The smaller the difference, the closest they are.
+			int idx_day = 0; //variable to save the index of the value in the array
+			int i = 0;
+   			for (int i; i < size_months; i++) { //for every value in the array
+       			if (abs(stoi(months_arr[i].substr(0,2)) - stoi(q.substr(0,2))) < diff_day) { //if the difference between the the element in the list 
+				//and the value is smaller than the current difference
+           			diff_day = abs(stoi(months_arr[i].substr(0,2)) - stoi(q.substr(0,2))); //save that value as the new difference
+            		idx_day = i; //save the index of the element in the array
+        		}
+    		}
+			return months_arr[idx_day]; // the closest element according to the year, month and day
+
+		}
+
+	}
 
 	/*
 	// Perform the Binary search to answer the queries
